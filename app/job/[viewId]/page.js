727a7b65e0bafd10cdccde31751c540fc4a25ae5
unknown <pebuka351@gmail.com>
2023-12-06 08:53:02 +0100

    Added Redirect Page

---
+++ app/job/[viewId]/page.js
@@ -0,0 +1,29 @@
+'use client'
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
+import axios from '@/app/lib/axios'
+
+export default function Page({ params }) {
+    const router = useRouter()
+
+    const slug = params.viewId
+
+    useEffect(() => {
+        if (slug) {
+            axios
+                .get(`/job/${slug}`)
+                .then(res => {
+                    const data = res.data
+
+                    router.push(`/jobs/${data.display}`)
+                })
+                .catch(error => {
+                    alert(error)
+                })
+        }
+    }, [slug, router])
+
+    // No need for a JSX return, as this page is intended for immediate redirection
+
+    return null
+}
