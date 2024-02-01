68c259acfaf61e7b919cbbe19f955ad6bb91031d
Tesimune <tesimune@gmail.com>
2024-02-01 03:22:52 +0100

    Updated cors

--- app/lib/axios.js
+++ app/lib/axios.js
@@ -3,7 +3,8 @@ import Axios from 'axios'
 const axios = Axios.create({
     baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
     headers: {
-        'X-Requested-With': 'XMLHttpRequest',
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
     },
     withCredentials: true,
 })
