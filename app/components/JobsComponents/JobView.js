4ea3d9364e72476abb31403c37fd9574e5d79a66
Promise Onuoha <pebuka351@gmail.com>
2024-03-19 16:48:26 +0100

    Update JobView.js

--- app/components/JobsComponents/JobView.js
+++ app/components/JobsComponents/JobView.js
@@ -183,7 +183,7 @@ export default function JobView() {
                                                             ? job
                                                                   .hiringOrganization
                                                                   .logo
-                                                            : '/images/jobs/compay_logo.JPG'
+                                                            : '/images/jobs/company_logo.JPG'
                                                     }
                                                     alt="LOGO"
                                                     className={`md:w-max md:h-[185px] object-contain w-[206px] h-[125px]`}
