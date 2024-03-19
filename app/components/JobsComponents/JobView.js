dc54719e345672785ee2c8469def818601a20f17
Promise Onuoha <pebuka351@gmail.com>
2024-03-19 16:43:56 +0100

    Updated company logo

--- app/components/JobsComponents/JobView.js
+++ app/components/JobsComponents/JobView.js
@@ -183,7 +183,7 @@ export default function JobView() {
                                                             ? job
                                                                   .hiringOrganization
                                                                   .logo
-                                                            : '/logo.png'
+                                                            : '/images/jobs/compay_logo.JPG'
                                                     }
                                                     alt="LOGO"
                                                     className={`md:w-max md:h-[185px] object-contain w-[206px] h-[125px]`}
