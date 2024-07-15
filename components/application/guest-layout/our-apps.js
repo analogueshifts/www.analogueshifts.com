69310e86415525d99ee5095bbefbc94a11136c3a
unknown <pebuka351@gmail.com>
2024-07-15 17:04:05 +0100

    Added Services to Navigation

--- components/application/guest-layout/our-apps.js
+++ components/application/guest-layout/our-apps.js
@@ -17,7 +17,7 @@ export default function OurApps() {
             <NavigationMenuList>
                 <NavigationMenuItem>
                     <NavigationMenuTrigger className="bg-transparent w-screen lg:w-auto justify-between lg:justify-center data-[active]:bg-transparent data-[state=open]:bg-transparent text-gray-600  lg:text-gray-500 lg:hover:text-gray-700 hover:bg-transparent focus:bg-transparent">
-                        <div className="inline-flex  items-center px-1 text-base lg:text-sm font-medium leading-5 focus:outline-none">
+                        <div className="inline-flex  items-center lg:px-1 text-base lg:text-sm font-medium leading-5 focus:outline-none">
                             <span className="hidden lg:block">Our</span>&nbsp;
                             Services
                         </div>
